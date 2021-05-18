f18539ad2cfc8a7db85dc7116ec71106229ecd98
kiranj1991 <84419234+kiranj1991@users.noreply.github.com>
2021-05-19 01:22:25 +0300

    initial commit

--- src/buyflow/SummaryStep.tsx
+++ src/buyflow/SummaryStep.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 interface SummaryStepProps {
   collectedData: {
-    email: string
-    age: number
-  }
+    name: {
+      firstName: string;
+      lastName: string;
+    };
+    email: string;
+    age: number;
+  };
 }
 
 const SummaryStep: React.FC<SummaryStepProps> = (props) => {
   return (
     <>
+      <div>First Name: {props.collectedData.name.firstName}</div>
+      <div>Last Name: {props.collectedData.name.lastName}</div>
       <div>Email: {props.collectedData.email}</div>
       <div>Age: {props.collectedData.age}</div>
       <div>
         <Link to="/purchased=dev_ins">Purchase</Link>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SummaryStep
+export default SummaryStep;
